bd5cb9ea7b9b8bc9497de8e8a8ebe978e075b826
Galin Velikov <102305995+galinvelikov@users.noreply.github.com>
2022-06-02 21:18:29 +0300

    Update devops.py

--- devops.py
+++ devops.py
@@ -1,3 +1,18 @@
-command = "Hello, these are my first steps in Jenkins as DevOps"
+#!/bin/bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
+###First DevOps try
+sudo systemctl enable jenkins
 
-print(command)
+#Docker
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
